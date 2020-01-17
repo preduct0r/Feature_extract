4c9d30b772a9969803724a1ff4736c8f55b69352
kotov <preductor@gmail.com>
2020-01-17 18:11:45 +0300

    находим некоррелированные фичи 17.01.20

--- top10_opensmile.py
+++ top10_opensmile.py
@@ -8,6 +8,8 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from lightgbm import LGBMClassifier
 from sklearn.metrics import f1_score, classification_report, precision_score
+from sklearn.metrics import pairwise_distances
+
 
 
 
@@ -25,29 +27,123 @@ with open(os.path.join(features_path, 'y_test.pkl'), 'rb') as f:
     y_test = pickle.load(f).loc[:, 'cur_label']
 
 
-# use lightgbm to get top-100 features
+
+#================================================================================================
+#================================================================================================
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import DBSCAN
+
+
+vecs = []
+for i in range(x_train.shape[1]):
+    vecs.append(x_train[:,i])
+corr_matrix = pd.DataFrame(np.corrcoef(vecs))
+
+# почему-то 0 и 1512 фича считаются некорректно
+def func(x):
+    return 1-abs(x)
+
+corr_matrix = func(corr_matrix.drop(columns=[0,1512], index=[0,1512]))
+
+
+# разбиваем фичи по кластерам
+feature_indexes = list(range(x_train.shape[1]))
+del feature_indexes[0]
+del feature_indexes[1511]
+
+
+
+# Делаем кастомную affinity func
+# def sim(x, y):
+#     return corr_matrix.loc[int(x[0]),int(y[0])]
+#
+# def sim_affinity(X):
+#     return pairwise_distances(X, metric=sim)
+#
+
+# # Обучаем Agglomerative Clustering с новым affinity
+# cluster = AgglomerativeClustering(n_clusters=10, affinity=sim_affinity, linkage='average')
+# cluster.fit(np.array(feature_indexes).reshape(-1, 1))
+
+# # Обучаем Agglomerative Clustering с "precomputed"
+# cluster = AgglomerativeClustering(n_clusters=10, affinity='precomputed', linkage='average')
+# cluster.fit(corr_matrix)
+
+# Обучаем DBSCAN с новым affinity
+cluster = DBSCAN(eps=0.03, min_samples=2, metric="precomputed")
+cluster.fit(corr_matrix)
+
+# Делаю сводную таблицу соответствия фичей и лэйблов
+clustering_labels = pd.DataFrame(columns=['feature', 'label'])
+clustering_labels['feature'] = feature_indexes
+clustering_labels['label'] = cluster.labels_
+
+
+temp = pd.DataFrame(columns=['label', 'value_count'])
+temp['value_count'] = clustering_labels.label.value_counts()
+temp['label'] = clustering_labels.label.value_counts().index
+
+
+
+# Находим самую репрезентативную фичу в каждом кластере, делаем сводную таблицу по кластерам
+# и матрицу корреляции для отобранных фичей
+best_indexes = []
+grouped = clustering_labels.groupby('label')
+df_clusters_info = pd.DataFrame(columns=['mean','max','std','label'])
+df_clusters_info['label'] = clustering_labels.label.unique()
+not_corr_idxs = []
+for name,group in grouped:
+   curr_features = group.feature.to_list()
+   group_corr_matrix = corr_matrix[corr_matrix.index.isin(curr_features)].loc[:,curr_features]
+   summ = group_corr_matrix.sum()
+   maxx = group_corr_matrix.max().max()
+   meann = group_corr_matrix.mean().mean()                                # для демонстрации каждого кластера
+   stdd = group_corr_matrix.std().mean()
+   curr_idx = df_clusters_info[df_clusters_info.label == name].index[0]
+   df_clusters_info.loc[curr_idx,:] = [round(meann,3), round(maxx,3), round(stdd,3), name]
+   max_index = summ.idxmax()
+   if name==-1:
+       not_corr_idxs = list(group_corr_matrix.index)
+   else:
+       best_indexes.append(max_index)
+best_indexes += not_corr_idxs
+best_vecs = [vecs[x] for x in best_indexes]
+zz = pd.DataFrame(data=abs(np.corrcoef(best_vecs)), index=best_indexes, columns=best_indexes)
+zz.to_csv(r"C:\Users\kotov-d\Documents\TASKS\task#7\best_features_corr_matrix.csv", index=True)
+df_clusters_info = df_clusters_info.merge(temp, how='left', on='label', left_index=True)
+df_clusters_info = df_clusters_info.loc[:,['label','value_count','max','mean','std']]
+df_clusters_info.to_csv(r"C:\Users\kotov-d\Documents\TASKS\task#7\df_clusters_info.csv", index=False)
+# print(pd.read_csv(r"C:\Users\kotov-d\Documents\TASKS\task#7\best_features_corr_matrix.csv", index_col=0).iloc[:10,:10])
+
+
+
+
+
+
+
+# use lightgbm to get top features
 #================================================================================================
 #================================================================================================
 
-clf = LGBMClassifier(boosting_type='gbdt', num_leaves=31, max_depth=-1, learning_rate=0.001, n_estimators=1000,
-                         objective=None, min_split_gain=0, min_child_weight=3, min_child_samples=10, subsample=0.8,
-                         subsample_freq=1, colsample_bytree=0.7, reg_alpha=0.3, reg_lambda=0, seed=17)
-
-clf.fit(x_train, y_train)
-
-
-dict_importance = {}
-for feature, importance in zip(range(len(clf.feature_importances_)), clf.feature_importances_):
-    dict_importance[feature] = importance
-
-best_features = []
-
-for idx, w in enumerate(sorted(dict_importance, key=dict_importance.get, reverse=True)):
-    if idx == 10:
-        break
-    best_features.append(w)
-
-print(best_features)
+# clf = LGBMClassifier(boosting_type='gbdt', num_leaves=31, max_depth=-1, learning_rate=0.001, n_estimators=1000,
+#                          objective=None, min_split_gain=0, min_child_weight=3, min_child_samples=10, subsample=0.8,
+#                          subsample_freq=1, colsample_bytree=0.7, reg_alpha=0.3, reg_lambda=0, seed=17)
+#
+# clf.fit(x_train, y_train)
+#
+#
+# dict_importance = {}
+# for feature, importance in zip(range(len(clf.feature_importances_)), clf.feature_importances_):
+#     dict_importance[feature] = importance
+#
+# best_features = []
+#
+# for idx, w in enumerate(sorted(dict_importance, key=dict_importance.get, reverse=True)):
+#     if idx == 10:
+#         break
+#     best_features.append(w)
+#
+# print(best_features)
 #
 # with open(os.path.join(r'C:\Users\kotov-d\Documents', 'clf' + '.pkl'), 'wb') as f:
 #     pickle.dump(clf, f, protocol=2)
@@ -61,25 +157,33 @@ print(best_features)
 #                                   y_test.values.reshape(-1,1))), columns=clf.classes_.tolist()+['pred','y']))
 
 
-# use mutual correlation to get rid of features
-#================================================================================================
-#================================================================================================
 
-from sklearn.cluster import AgglomerativeClustering
-from scipy.cluster.hierarchy import linkage
-from scipy.cluster.hierarchy import dendrogram
+# ====================================================================================================================
+# ====================================================================================================================
+# проверка полученных результатов с помощью усреднения значений ячеек таблицы корреляции
+best_matrix = corr_matrix[corr_matrix.index.isin(best_indexes)]
+print((best_matrix.sum().sum()-best_matrix.shape[0])/(best_matrix.shape[0]**2-best_matrix.shape[0]))
+print((corr_matrix.sum().sum()-corr_matrix.shape[0])/(corr_matrix.shape[0]**2-corr_matrix.shape[0]))
 
-vecs = []
-for i in range(x_train.shape[1]):
-    vecs.append(x_train[:,i])
-corr_matrix = pd.DataFrame(np.corrcoef(vecs))
 
-# почему-то 0 и 1512 фича считаются некорректно
-def func(x):
-    return 1-x
 
-corr_matrix = func(abs(corr_matrix.drop(columns=[0,1512], index=[0,1512])))
+# проверка с помощью обучения модели на разных фичах
+from sklearn.svm import SVC
+clf=SVC()
+pred = clf.fit(x_train[:,best_indexes], y_train).predict(x_test[:,best_indexes])
+print("на фичах полученных при помощи корреляции f1 macro {}".format(round(f1_score(y_test, pred, average='macro'),3)))
 
+pred = clf.fit(x_train[:,best_features], y_train).predict(x_test[:,best_features])
+print("на фичах полученных при помощи feature_importances f1 macro {}".format(round(f1_score(y_test, pred, average='macro'),3)))
+
+pred = clf.fit(x_train, y_train).predict(x_test)
+print("на оригинальных фичах f1 macro {}".format(round(f1_score(y_test, pred, average='macro'),3)))
+
+
+
+# возможно прегодится
+# ====================================================================================================================
+# ====================================================================================================================
 
 # # смотрим сколько значений корреляции выше трешхолда
 # threshold = 0.9
@@ -93,59 +197,3 @@ corr_matrix = func(abs(corr_matrix.drop(columns=[0,1512], index=[0,1512])))
 # plt.figure(figsize=(25, 25))
 # dn = dendrogram(Z)
 # plt.show()
-
-
-
-# разбиваем фичи по кластерам
-feature_indexes = list(range(x_train.shape[1]))
-del feature_indexes[0]
-del feature_indexes[1512]
-
-clustering = AgglomerativeClustering(n_clusters=10).fit(corr_matrix)
-
-clustering_labels = pd.DataFrame(columns=['feature', 'label'])
-clustering_labels['feature'] = feature_indexes
-clustering_labels['label'] = clustering.labels_
-with open(r"C:\Users\kotov-d\Documents\TASKS\top_features\clustering_labels.pkl", "wb") as f:
-    pickle.dump(clustering_labels, f)
-
-
-
-
-# находим индексы лучших фичей
-with open(r"C:\Users\kotov-d\Documents\TASKS\top_features\clustering_labels.pkl", "rb") as f:
-    clustering_labels = pickle.load(f)
-
-best_indexes = []
-
-grouped = clustering_labels.groupby('label')
-for name,group in grouped:
-   curr_features = group.feature.to_list()
-   group_corr_matrix = corr_matrix[corr_matrix.index.isin(curr_features)].loc[:,curr_features]
-   group_corr_matrix = group_corr_matrix.assign(summ=lambda x: group_corr_matrix.sum())
-   max_index = group_corr_matrix.summ.idxmax()
-   best_indexes.append(max_index)
-
-print(best_indexes)
-
-
-
-# проверка полученных результатов
-best_matrix = corr_matrix[corr_matrix.index.isin(best_indexes)]
-# print(best_matrix)
-print((best_matrix.sum().sum()-best_matrix.shape[0])/(best_matrix.shape[0]**2-best_matrix.shape[0]))
-print((corr_matrix.sum().sum()-corr_matrix.shape[0])/(corr_matrix.shape[0]**2-corr_matrix.shape[0]))
-
-
-
-# проверка с помощью обучения модели
-from sklearn.svm import SVC
-clf=SVC()
-pred = clf.fit(x_train[:,best_indexes], y_train).predict(x_test[:,best_indexes])
-print("на фичах полученных при помощи корреляции f1 macro {}".format(round(f1_score(y_test, pred, average='macro'),3)))
-
-pred = clf.fit(x_train[:,best_features], y_train).predict(x_test[:,best_features])
-print("на фичах полученных при помощи feature_importances f1 macro {}".format(round(f1_score(y_test, pred, average='macro'),3)))
-
-
-
