0ae71b5fc268db2c054fe770487f4ac3a34ba3b2
kotov <preductor@gmail.com>
2020-01-17 19:46:54 +0300

    gridsearch попробовать, pairwise учитывает ли диагонали

--- top10_opensmile.py
+++ top10_opensmile.py
@@ -18,14 +18,26 @@ features_path = os.path.join(base_dir, 'telecom_vad', 'feature', 'opensmile')
 
 
 with open(os.path.join(features_path, 'x_train.pkl'), 'rb') as f:
-    x_train = np.vstack(pickle.load(f))
+    x_train = pd.DataFrame(np.vstack(pickle.load(f)))
 with open(os.path.join(features_path, 'x_test.pkl'), 'rb') as f:
-    x_test = np.vstack(pickle.load(f))
+    x_test = pd.DataFrame(np.vstack(pickle.load(f)))
 with open(os.path.join(features_path, 'y_train.pkl'), 'rb') as f:
     y_train = pickle.load(f).loc[:, 'cur_label']
 with open(os.path.join(features_path, 'y_test.pkl'), 'rb') as f:
     y_test = pickle.load(f).loc[:, 'cur_label']
 
+
+
+x_train['target'] = y_train
+x_test['target'] = y_test
+
+x_train = x_train[x_train.target!='defective'][x_train.target!='sad'][x_train.target!='not_ informative']
+y_train = x_train.target
+x_train.drop(columns=['target'], inplace=True)
+
+x_test = x_test[x_test.target!='defective'][x_test.target!='sad'][x_test.target!='not_ informative']
+y_test = x_test.target
+x_test.drop(columns=['target'], inplace=True)
 
 
 #================================================================================================
@@ -36,7 +48,7 @@ from sklearn.cluster import DBSCAN
 
 vecs = []
 for i in range(x_train.shape[1]):
-    vecs.append(x_train[:,i])
+    vecs.append(x_train.iloc[:,i])
 corr_matrix = pd.DataFrame(np.corrcoef(vecs))
 
 # почему-то 0 и 1512 фича считаются некорректно
@@ -124,32 +136,37 @@ df_clusters_info.to_csv(r"C:\Users\kotov-d\Documents\TASKS\task#7\df_clusters_in
 # use lightgbm to get top features
 #================================================================================================
 #================================================================================================
+cur_pred = 0
+def test_100_features(x_train, y_train, x_test, y_test):
+    clf = LGBMClassifier(boosting_type='gbdt', num_leaves=31, max_depth=-1, learning_rate=0.001, n_estimators=1000,
+                             objective=None, min_split_gain=0, min_child_weight=3, min_child_samples=10, subsample=0.8,
+                             subsample_freq=1, colsample_bytree=0.7, reg_alpha=0.3, reg_lambda=0, seed=17)
+    clf.fit(x_train, y_train)
 
-# clf = LGBMClassifier(boosting_type='gbdt', num_leaves=31, max_depth=-1, learning_rate=0.001, n_estimators=1000,
-#                          objective=None, min_split_gain=0, min_child_weight=3, min_child_samples=10, subsample=0.8,
-#                          subsample_freq=1, colsample_bytree=0.7, reg_alpha=0.3, reg_lambda=0, seed=17)
-#
-# clf.fit(x_train, y_train)
-#
-#
-# dict_importance = {}
-# for feature, importance in zip(range(len(clf.feature_importances_)), clf.feature_importances_):
-#     dict_importance[feature] = importance
-#
-# best_features = []
-#
-# for idx, w in enumerate(sorted(dict_importance, key=dict_importance.get, reverse=True)):
-#     if idx == 10:
-#         break
-#     best_features.append(w)
-#
-# print(best_features)
-#
-# with open(os.path.join(r'C:\Users\kotov-d\Documents', 'clf' + '.pkl'), 'wb') as f:
-#     pickle.dump(clf, f, protocol=2)
-#
-# # with open(os.path.join(r'C:\Users\kotov-d\Documents', 'clf' + '.pkl'), 'rb') as f:
-# #     clf = pickle.load(f)
+    dict_importance = {}
+    for feature, importance in zip(range(len(clf.feature_importances_)), clf.feature_importances_):
+        dict_importance[feature] = importance
+
+    best_lgbm_features = []
+
+    for idx, w in enumerate(sorted(dict_importance, key=dict_importance.get, reverse=True)):
+        if idx == 1:
+            break
+        best_lgbm_features.append(w)
+
+    clf.fit(x_train.iloc[:,best_lgbm_features], y_train)
+
+    pred = clf.predict(x_test.iloc[:,best_lgbm_features])
+
+
+    print(round(f1_score(y_test, pred, average='macro'),30))
+
+test_100_features(x_train, y_train, x_test, y_test)
+test_100_features(x_train.iloc[:,best_indexes], y_train, x_test.iloc[:,best_indexes], y_test)
+print(y_train.value_counts())
+
+
+
 
 # # вывод предсказанных и реальных значений
 # print(clf.classes_.tolist()+['pred','y'])
@@ -160,21 +177,21 @@ df_clusters_info.to_csv(r"C:\Users\kotov-d\Documents\TASKS\task#7\df_clusters_in
 
 # ====================================================================================================================
 # ====================================================================================================================
-# проверка полученных результатов с помощью усреднения значений ячеек таблицы корреляции
-best_matrix = corr_matrix[corr_matrix.index.isin(best_indexes)]
-print((best_matrix.sum().sum()-best_matrix.shape[0])/(best_matrix.shape[0]**2-best_matrix.shape[0]))
-print((corr_matrix.sum().sum()-corr_matrix.shape[0])/(corr_matrix.shape[0]**2-corr_matrix.shape[0]))
+# # проверка полученных результатов с помощью усреднения значений ячеек таблицы корреляции
+# best_matrix = corr_matrix[corr_matrix.index.isin(best_indexes)]
+# print((best_matrix.sum().sum()-best_matrix.shape[0])/(best_matrix.shape[0]**2-best_matrix.shape[0]))
+# print((corr_matrix.sum().sum()-corr_matrix.shape[0])/(corr_matrix.shape[0]**2-corr_matrix.shape[0]))
 
 
 
 # проверка с помощью обучения модели на разных фичах
 from sklearn.svm import SVC
 clf=SVC()
-pred = clf.fit(x_train[:,best_indexes], y_train).predict(x_test[:,best_indexes])
+pred = clf.fit(x_train.iloc[:,best_indexes], y_train).predict(x_test.iloc[:,best_indexes])
 print("на фичах полученных при помощи корреляции f1 macro {}".format(round(f1_score(y_test, pred, average='macro'),3)))
 
-pred = clf.fit(x_train[:,best_features], y_train).predict(x_test[:,best_features])
-print("на фичах полученных при помощи feature_importances f1 macro {}".format(round(f1_score(y_test, pred, average='macro'),3)))
+# pred = clf.fit(x_train[:,best_features], y_train).predict(x_test[:,best_features])
+# print("на фичах полученных при помощи feature_importances f1 macro {}".format(round(f1_score(y_test, pred, average='macro'),3)))
 
 pred = clf.fit(x_train, y_train).predict(x_test)
 print("на оригинальных фичах f1 macro {}".format(round(f1_score(y_test, pred, average='macro'),3)))
